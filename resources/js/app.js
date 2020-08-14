d0753ab9f90a75763099f2fc7038dec707b3607a
rjRenolo <rjrenolo14@gmail.com>
2020-08-14 15:12:24 +0800

    Initiali setup for Laravel-Vue-Tailwind

---
+++ resources/js/app.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+// import VueRouter from "vue-router";
+
+import App from "./components/App";
+
+import router from "./router";
+
+require("./bootstrap");
+
+const app = new Vue({
+    el: "#app",
+    components: {
+        App,
+    },
+    router,
+});
